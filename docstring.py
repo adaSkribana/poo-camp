0232526645cef402cda0413667ee9b5896ace78b
adaSkribana <136006242+adaSkribana@users.noreply.github.com>
2024-02-22 13:09:55 -0300

    docstring poo

---
+++ docstring.py
@@ -0,0 +1,20 @@
+defina los siguientes conceptos y/o su uso:
+Clase: Una clase es un molde que define las propiedades (llamese atributos)
+    y los comportamientos (llamese metodo) que tendran los objetos creados a partir de ella
+Objeto: Un objeto es una instancia concreta de una clase, es decir que cuando creamos un objeto
+    estamos creando una copia especifica basada en este molde
+Metodo: Un método es una función o procedimiento (llamese serie de instrucciones) definido dentro de una clase.
+    Representa una acción que los objetos de esa clase pueden realizar
+Instancia: Una instancia es un objeto especifico creado a partir de una clase
+    cuando creamos un objeto entamos instanciando esa clase.
+Metodo de clase: Un método de clase (también llamado método estático) es un método que pertenece a la clase en sí,
+    no a una instancia particular.
+    Puede ser invocado directamente desde la clase, sin necesidad de crear un objeto.
+Metodo de instancia: Un método de instancia es un método que opera sobre un objeto específico creado a partir de la clase.
+    Se invoca en una instancia particular y puede acceder a sus atributos.
+Variables de instancia: Las variables de instancia son atributos específicos de un objeto.
+    Cada instancia tiene sus propias variables de instancia con valores independientes.
+Variables de clase: Las variables de clase (también llamadas variables estáticas) son compartidas por todas las instancias de una clase.
+    Su valor es el mismo para todos los objetos creados a partir de esa clase.
+Getters y Setters: Los getters y setters son métodos especiales utilizados para acceder y modificar los valores de las variables de instancia.
+    Los getters obtienen el valor de una variable, y los setters lo modifican. 
